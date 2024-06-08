381a927b9ab86487fadd361ef33599b573091ab4
EfehanSenturk <efehansenturk34@gmail.com>
2024-06-08 14:10:23 +0300

    Initial Commit

---
+++ DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by Efehan Şentürk on 8.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
